1f48f529d75851eea27836261fb90fc428218639
vidyakore <vidyakore3108@gmail.com>
2022-01-07 17:42:25 +0530

    can get the bytecode

---
+++ web3_py_simple_storage/SimpleStorage.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract first {
+
+    uint256 public f;
+    //this will initialize to 0 
+
+    struct People {
+        uint256 fNo;
+        string name;
+    }
+
+    // people public person = people({fNo:1,name:"vidya"});
+
+    function store(uint256 _f) public{
+        f = _f;
+    }
+
+    //array syntax is "type of array" "visibilty of array" "name of the array"
+
+    //dynamic array - can change size
+    People[] public peopleArr;
+    mapping(string => uint256) public nametofNo;
+
+    //view and pure functions are not state changing functions therefore they are blue and not orange
+
+    function retrieve() public view returns(uint256) {
+        return f;
+    }
+
+    // function retrieve(uint256 f) public pure {
+    //     f+f;
+    // }
+
+    function addPerson(string memory _fName, uint256 _fNo) public{
+        peopleArr.push(People({fNo:_fNo,name : _fName}));
+        nametofNo[_fName] = _fNo;
+    }
+
+    
+}
---
+++ web3_py_simple_storage/deploy.py
@@ -0,0 +1,38 @@
+from solcx import compile_standard
+# solcx.install_solc('0.6.0')
+# install_solc('v0.4.25')
+with open("SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+# print(simple_storage_file)
+
+# compiling solidity
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+# compiled_sol = compile_standard(
+#     {
+#         "language": "Solidity",
+#         "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+#         "settings": {
+#             "outputSelection": {
+#                 "*": {
+#                     "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+#                 }
+#             }
+#         },
+#     },
+#     solc_version="0.6.0",
+# )
+print(compiled_sol)
