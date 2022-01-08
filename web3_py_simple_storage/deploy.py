4028113b85704faf16c72c90a13df70a31f33ad0
vidyakore <vidyakore3108@gmail.com>
2022-01-08 12:55:37 +0530

    json file created contains ABI

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -1,4 +1,5 @@
 from solcx import compile_standard
+import json
 # solcx.install_solc('0.6.0')
 # install_solc('v0.4.25')
 with open("SimpleStorage.sol", "r") as file:
@@ -21,18 +22,7 @@ compiled_sol = compile_standard(
     solc_version="0.6.0",
 )
 
-# compiled_sol = compile_standard(
-#     {
-#         "language": "Solidity",
-#         "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
-#         "settings": {
-#             "outputSelection": {
-#                 "*": {
-#                     "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
-#                 }
-#             }
-#         },
-#     },
-#     solc_version="0.6.0",
-# )
-print(compiled_sol)
+# print(compiled_sol)
+
+with open("compiled_code.json","w+") as file:
+    json.dump(compiled_sol, file)
